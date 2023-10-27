b5b22f354fe4c196d2d6d02de339f73e73b83a43
Ayan <abc@gmail.com>
2023-10-27 22:35:26 +0530

    Iteration 2 loose coupling through GamingConsole interface

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -2,13 +2,16 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.PacManGame;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
         var marioGame = new MarioGame();
         var superContraGme = new SuperContraGame();
-        var gameRunner = new GameRunner(marioGame);
+        var pacManGame = new PacManGame();
+        var gameRunner = new GameRunner(pacManGame);
+
         gameRunner.run();
     }
 }
