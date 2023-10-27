02b0d51571cd80f82c27ee4427cd92a75a35bc7d
Ayan <abc@gmail.com>
2023-10-27 22:22:13 +0530

    game is tightly coupled to GameRunner class

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
@@ -0,0 +1,19 @@
+package com.in28minutes.learnspringframework.game;
+
+public class MarioGame {
+    public void up(){
+        System.out.println("jump");
+    }
+
+    public void down(){
+        System.out.println("go into hole");
+    }
+
+    public void left(){
+        System.out.println("go back");
+    }
+    public void right(){
+        System.out.println("go forward");
+    }
+
+}
